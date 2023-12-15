f19ca0d00f81ce422e2fcdcacee167eb69481e28
Maria J <cadking@gte.net>
2023-12-15 09:02:11 -0800

    db corr

--- src/main/java/com/mariaj/onestopmedishop/controllers/HomeController.java
+++ src/main/java/com/mariaj/onestopmedishop/controllers/HomeController.java
@@ -1,12 +1,12 @@
 package com.mariaj.onestopmedishop.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
